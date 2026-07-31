5ba56e6b5c7cab33ddf0dd64ad55abd6b5f960f1
3xacT <vovaoliynyk99@gmail.com>
2026-07-31 15:09:07 +0300

    fix review errors

--- spring-boot-adventure/src/main/java/org/example/springbootadventure/controller/BookController.java
+++ spring-boot-adventure/src/main/java/org/example/springbootadventure/controller/BookController.java
@@ -5,16 +5,18 @@ import lombok.RequiredArgsConstructor;
 import org.example.springbootadventure.dto.BookDto;
 import org.example.springbootadventure.dto.CreateBookRequestDto;
 import org.example.springbootadventure.service.BookService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/books")
+@RequestMapping("/books")
 public class BookController {
     private final BookService bookService;
 
@@ -28,6 +30,7 @@ public class BookController {
         return bookService.getBookById(id);
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.createBook(bookDto);

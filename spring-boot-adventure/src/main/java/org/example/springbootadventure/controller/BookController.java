ed7f09a7ef02a4d0f3d523abc8554dde8082b6e3
3xacT <vovaoliynyk99@gmail.com>
2026-08-07 12:58:42 +0300

    Update spring-boot-adventure/src/main/java/org/example/springbootadventure/controller/BookController.java
    Co-authored-by: Olena Bruiako <bruyako.elena@gmail.com>

--- spring-boot-adventure/src/main/java/org/example/springbootadventure/controller/BookController.java
+++ spring-boot-adventure/src/main/java/org/example/springbootadventure/controller/BookController.java
@@ -42,7 +42,7 @@ public class BookController {
     }
 
     @PutMapping("/{id}")
-    public BookDto updateBook(@PathVariable Long id,@RequestBody CreateBookRequestDto requestDto) {
+    public BookDto updateBook(@PathVariable Long id,@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.updateBook(id,requestDto);
     }
 

a951805e14afecdc36b2eadbedbdb506fad6961a
3xacT <vovaoliynyk99@gmail.com>
2026-08-16 20:37:49 +0300

    add jwt support

--- spring-boot-adventure/src/main/java/org/example/springbootadventure/dto/user/UserLoginRequestDto.java
+++ spring-boot-adventure/src/main/java/org/example/springbootadventure/dto/user/UserLoginRequestDto.java
@@ -3,15 +3,16 @@ package org.example.springbootadventure.dto.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 
-public record UserLoginRequestDto(
+@Data
+public class UserLoginRequestDto {
         @NotBlank
         @Email
         @Size(min = 4, max = 50)
-        String email,
+        String email;
 
         @NotBlank
         @Size(min = 6, max = 100)
-        String password
-) {
+        String password;
 }

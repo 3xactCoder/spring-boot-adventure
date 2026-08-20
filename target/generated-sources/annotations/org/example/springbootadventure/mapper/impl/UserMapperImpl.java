898be62d56a68ab70095de723f20d009299a50d9
3xacT <vovaoliynyk99@gmail.com>
2026-08-20 14:05:51 +0300

    fix project structure commit2

--- target/generated-sources/annotations/org/example/springbootadventure/mapper/impl/UserMapperImpl.java
+++ target/generated-sources/annotations/org/example/springbootadventure/mapper/impl/UserMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-08-20T14:01:28+0300",
+    date = "2026-08-20T14:05:31+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 25.0.2 (Oracle Corporation)"
 )
 @Component
